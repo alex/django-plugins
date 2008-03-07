7f0d2b5fda8839f2996c53aaa5d5eeb8693f27b4
Alex Gaynor <alex@alex-desktop-linux.(none)>
2008-03-07 12:03:35 -0600

    Fixed a few things with the template tag, still not completely working

--- plugins/templatetags/plugin.py
+++ plugins/templatetags/plugin.py
@@ -1,8 +1,8 @@
-from django.template import Library, Node
+from django import template
 
-from plugin.models import Plugin
+from plugins.models import Plugin
 
-register = Library()
+register = template.Library()
 
 """
 Usage:
@@ -11,12 +11,12 @@ or
 {% apply_plugin some_obj as new_obj %}
 """
 
-class ApplyPluginNode(Node):
+class ApplyPluginNode(template.Node):
     def __init__(self, object_name, varname=None):
         self.object_name, self.varname = object_name, varname
     
     def render(self, context):
-        obj = context[object_name]
+        obj = context[self.object_name]
         plgs = Plugin.objects.filter(active=True, acts_on=obj.__class__.__name__)
         for plg in plgs:
             plg_cls = plg.get_class()
@@ -33,10 +33,10 @@ def apply_plugin(parser, token):
     bits = token.contents.split()
     if len(bits) == 4:
         if bits[2] != 'as':
-            raise TemplateSyntaxError, "apply_plugin tag's second argument must be 'as'"
+            raise template.TemplateSyntaxError, "apply_plugin tag's second argument must be 'as'"
         return ApplyPluginNode(bits[1], bits[3])
-    if len(bits) == 2:
+    elif len(bits) == 2:
         return ApplyPluginNode(bits[1])
     else:
-        raise TemplateSyntaxError, "apply_plugin tag takes either 1 or 3 arguments"
-apply_plugin = register.tag(apply_plugin)
+        raise template.TemplateSyntaxError, "apply_plugin tag takes either 1 or 3 arguments"
+register.tag(apply_plugin.__name__, apply_plugin)
